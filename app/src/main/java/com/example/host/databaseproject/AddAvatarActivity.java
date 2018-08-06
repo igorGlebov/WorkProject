09c4f49b36451771f0acf1a1e05e7b5a6de82a4c
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-07 00:46:08 +0300

    FileCommit

--- app/src/main/java/com/example/host/databaseproject/AddAvatarActivity.java
+++ app/src/main/java/com/example/host/databaseproject/AddAvatarActivity.java
@@ -2,12 +2,25 @@ package com.example.host.databaseproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddAvatarActivity extends AppCompatActivity {
+
+    private Button addAvatarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_avatar);
+
+        addAvatarButton = findViewById(R.id.addAvatarButton);
+
+        addAvatarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
