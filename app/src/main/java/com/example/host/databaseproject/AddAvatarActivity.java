4c7ef565b46713b76184c089b933289c4166ef17
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-07 01:01:31 +0300

    CreateImage

--- app/src/main/java/com/example/host/databaseproject/AddAvatarActivity.java
+++ app/src/main/java/com/example/host/databaseproject/AddAvatarActivity.java
@@ -1,13 +1,20 @@
 package com.example.host.databaseproject;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class AddAvatarActivity extends AppCompatActivity {
 
     private Button addAvatarButton;
+    private static final int CAMERA_REQUEST = 0;
+    private ImageView imageAvatar; // фотка
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +22,23 @@ public class AddAvatarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_avatar);
 
         addAvatarButton = findViewById(R.id.addAvatarButton);
+        imageAvatar = findViewById(R.id.imageView);
 
         addAvatarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            // Фотка сделана, извлекаем картинку
+            Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
+            imageAvatar.setImageBitmap(thumbnailBitmap);
+        }
     }
 }
