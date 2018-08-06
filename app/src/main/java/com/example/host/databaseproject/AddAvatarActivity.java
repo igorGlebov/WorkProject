48868a1555bbbfe4bf612584bdd0e12986931136
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-06 22:10:12 +0300

    DeleteCommentCommit

---
+++ app/src/main/java/com/example/host/databaseproject/AddAvatarActivity.java
@@ -0,0 +1,13 @@
+package com.example.host.databaseproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddAvatarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_avatar);
+    }
+}
