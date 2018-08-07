c58cbf6bc645e9bb5e5283a320769de42f24109c
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-07 18:56:20 +0300

    Кое что переделал, xml файл не нужен

--- app/src/main/java/com/example/host/databaseproject/CustomDialogFragment.java
+++ app/src/main/java/com/example/host/databaseproject/CustomDialogFragment.java
@@ -2,19 +2,34 @@ package com.example.host.databaseproject;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.DialogFragment;
 
 public class CustomDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        String[] actions = {"Сделать фото", "Выбрать фото из галереи"};
         return builder
                 .setTitle("Диалоговое окно")
                 .setIcon(android.R.drawable.ic_dialog_alert)
-                .setView(R.layout.dialog)
-                .setNeutralButton("Нажать", null)
+                .setItems(actions, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(which == 0){
+
+                        }
+                        else{
+
+                        }
+                    }
+                })
                 .create();
         //return super.onCreateDialog(savedInstanceState);
     }
+
+
 }
