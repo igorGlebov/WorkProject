f7f5cb2835f71113ddb106feed90558860aecd0d
RaStABr0 <dr.v991149@gmail.com>
2018-08-21 18:19:10 +0300

    Юзер коммит

---
+++ app/src/main/java/com/example/host/databaseproject/OurClasses/User.java
@@ -0,0 +1,81 @@
+package com.example.host.databaseproject.OurClasses;
+
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
+public class  User implements Serializable {
+    private String email;
+    private String name;
+    private String surname;
+    private String fatherName;
+    private Bitmap avatar;
+    private String userID;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public Bitmap getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(Bitmap avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public User(String email, String name, String surname, String fatherName, String userID){
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.fatherName = fatherName;
+        this.userID = userID;
+    }
+
+    public User(String email, String name, String surname, String fatherName, String userID, Bitmap avatar){
+        this.email = email;
+        this.name = name;
+        this.surname = surname;
+        this.fatherName = fatherName;
+        this.userID = userID;
+        this.avatar = avatar;
+    }
+
+}
