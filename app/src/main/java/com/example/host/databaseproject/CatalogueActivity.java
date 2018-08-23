d5932ec9c74b3dca1abdcaf30013b7d308dfe29d
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-23 18:57:38 +0300

    Переделал. Можешь вообще пока не трогать. С двумя фрагментами все работает(ВРОДЕ ...).

--- app/src/main/java/com/example/host/databaseproject/CatalogueActivity.java
+++ app/src/main/java/com/example/host/databaseproject/CatalogueActivity.java
@@ -1,6 +1,5 @@
 package com.example.host.databaseproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,32 +12,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class CatalogueActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView = null;
+    Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalogue);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //Set the fragment initially
+        BlankFragment fragment = new BlankFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Тут будет корзина", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        //navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        //How to change elements in the header programatically
+//        View headerView = navigationView.getHeaderView(0);
+//        TextView emailText = (TextView) headerView.findViewById(R.id.email);
+//        emailText.setText("newemail@email.com");
+
+        //navigationView.setNavigationItemSelectedListener(this);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -56,7 +76,7 @@ public class CatalogueActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.catalogue, menu);
+        getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
     }
 
@@ -81,14 +101,24 @@ public class CatalogueActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.settings) {
-            startActivity(new Intent(CatalogueActivity.this, SettingsActivity2.class));
-        }
-        else if(id == R.id.katalog) {
+        if (id == R.id.katalog) {
+            //Set the fragment initially
+            BlankFragment fragment = new BlankFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            // Handle the camera action
+        } else if (id == R.id.settings) {
+            //Set the fragment initially
+            BlankFragment2 fragment = new BlankFragment2 ();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
 
-        }
-        else if(id == R.id.logOut) {
-            startActivity(new Intent(CatalogueActivity.this, LoginActivity.class));
+        } else if (id == R.id.logOut) {
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -96,3 +126,155 @@ public class CatalogueActivity extends AppCompatActivity
         return true;
     }
 }
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.view.View;
+//import android.support.design.widget.NavigationView;
+//import android.support.v4.view.GravityCompat;
+//import android.support.v4.widget.DrawerLayout;
+//import android.support.v7.app.ActionBarDrawerToggle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+//public class CatalogueActivity extends AppCompatActivity
+//        implements NavigationView.OnNavigationItemSelectedListener {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_catalogue);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Тут будет корзина", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+////        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+////        ft.replace(R.id.content, new BlankFragment()).commit();
+//        //displayView(R.id.katalog); // что будет по умолчанию стоять
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.catalogue, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//
+//        //displayView(item.getItemId());
+//        Fragment fragment = null;
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.settings) {
+//            fragment = new BlankFragment();
+//        } else if (id == R.id.katalog){
+//            fragment = new BlankFragment2();
+//        }
+//
+//        if (fragment != null) {
+//            FragmentManager fragmentManager = getSupportFragmentManager();
+//            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+//
+//        }
+//        return true;
+////        int id = item.getItemId();
+////
+////
+////        else if(id == R.id.katalog) {
+////
+////        }
+////        else if(id == R.id.logOut) {
+////            startActivity(new Intent(CatalogueActivity.this, LoginActivity.class));
+////        }
+////
+////        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+////        drawer.closeDrawer(GravityCompat.START);
+////        return true;
+//    }
+//
+////    public void displayView(int viewId) {
+////
+////        Fragment fragment = null;
+////        String title = getString(R.string.app_name);
+////
+////        switch (viewId) {
+////            case R.id.settings:
+////                fragment = new BlankFragment();
+////                title  = "News";
+////                break;
+////            case R.id.katalog:
+////                fragment = new BlankFragment2();
+////                title = "Events";
+////                break;
+////
+////        }
+////
+////        if (fragment != null) {
+////            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+////            ft.replace(R.id.content_frame, fragment);
+////            ft.commit();
+////        }
+////
+////        // set the toolbar title
+////        if (getSupportActionBar() != null) {
+////            getSupportActionBar().setTitle(title);
+////        }
+////
+////        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+////        drawer.closeDrawer(GravityCompat.START);
+////
+////    }
+//}
