e977a00eb8fc3637a9bcaa629ee8d426f4a6e269
RaStABr0 <dr.v991149@gmail.com>
2018-08-24 02:09:39 +0300

    Добавлена смена ФИО. Добавлена смена аватара. Добавлена смена емейла. Добавлена смена пароля. Ничего не тестилось, ибо не собирается, совершенно не удивлюсь, если все отвалится.

--- app/src/main/java/com/example/host/databaseproject/ChangePasswordActivity.java
+++ app/src/main/java/com/example/host/databaseproject/ChangePasswordActivity.java
@@ -1,5 +1,6 @@
 package com.example.host.databaseproject;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,12 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.host.databaseproject.OurClasses.Check;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -24,9 +30,14 @@ public class ChangePasswordActivity extends AppCompatActivity {
 
     private Button applyNewPasswordButton;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_change_password);
 
         oldPasswordText = findViewById(R.id.changePasswordOldPasswordText);
@@ -37,46 +48,59 @@ public class ChangePasswordActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
 
-
         applyNewPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!checkOldPassword()){
-                    return;
-                }
-                if(!Check.comparePassword(newPasswordText.getText().toString(), newPasswordText2.getText().toString())){
-                    Toast.makeText(ChangePasswordActivity.this, "Новые пароли не совпадают", Toast.LENGTH_LONG);
-                    return;
-                }
-                if(!Check.checkPassword(newPasswordText.getText().toString())){
-                    Toast.makeText(ChangePasswordActivity.this, "Пароль слишком простой", Toast.LENGTH_LONG);
-                    return;
-                }
-                user.updatePassword(newPasswordText.getText().toString()).addOnFailureListener(new OnFailureListener() {
+                AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), oldPasswordText.getText().toString());
+                user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(ChangePasswordActivity.this, "Не удалось сменить пароль. Проверьте соединение и повторите попытку", Toast.LENGTH_LONG);
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            if(!Check.comparePassword(newPasswordText.getText().toString(), newPasswordText2.getText().toString())){
+                                Toast.makeText(ChangePasswordActivity.this, "Новые пароли не совпадают", Toast.LENGTH_LONG);
+
+                            }
+
+                            if(!Check.checkPassword(newPasswordText.getText().toString())) {
+                                Toast.makeText(ChangePasswordActivity.this, "Пароль слишком простой", Toast.LENGTH_LONG);
+                                return;
+                            }
+
+
+                        }
+                        else {
+                            Toast.makeText(ChangePasswordActivity.this, "Старый пароль введен неверно", Toast.LENGTH_LONG);
+
+                        }
                     }
                 });
+
+
+                user.updatePassword(newPasswordText.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ChangePasswordActivity.this, "Пароль был изменен успешно!", Toast.LENGTH_LONG);
+                            startActivity(new Intent(ChangePasswordActivity.this, SettingsActivity2.class));
+
+                        }
+                        else{
+                            Toast.makeText(ChangePasswordActivity.this, "Не удалось сменить пароль. Проверьте соединение и повторите попытку", Toast.LENGTH_LONG);
+
+                        }
+                    }
+                });
+
             }
         });
-    }
 
-    private boolean checkOldPassword(){
-        //хз почему так, по- ругался
-        final boolean[] result = {true};
-        mAuth.signInWithEmailAndPassword(user.getEmail(), oldPasswordText.getText().toString()).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ChangePasswordActivity.this, "Старый пароль введен неверно!", Toast.LENGTH_LONG);
-                result[0] = false;
 
-            }
 
-        });
-        return result[0];
 
     }
+
+
+
 }
 
 
--- app/src/main/java/com/example/host/databaseproject/SettingsActivity2.java
+++ app/src/main/java/com/example/host/databaseproject/SettingsActivity2.java
@@ -2,6 +2,10 @@ package com.example.host.databaseproject;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +17,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.host.databaseproject.OurClasses.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,23 +27,45 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StreamDownloadTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 
-public class SettingsActivity2 extends AppCompatActivity {
+public class SettingsActivity2 extends AppCompatActivity implements Datable {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+    private FirebaseStorage storage;
+
+    private StorageReference storageReference;
+    private DatabaseReference userReference;
+
 
     private Button changePasswordButton;
     private ImageButton changeAvatarButton;
 
 
 
+    //For avatar change
+    private static final int CAMERA_REQUEST = 0;
+    static final int GALLERY_REQUEST = 1;
+    private ImageView imageAvatar; // фотка
+    //
+
+    private String name, surname, fatherName, email;
+
+
     //private ImageView avatarView;
     private EditText nameView;
+    private EditText surnameView;
+    private EditText fatherNameView;
     private EditText emailView;
-    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,20 +78,27 @@ public class SettingsActivity2 extends AppCompatActivity {
 
         //avatarView = findViewById(R.id.settingsAvatarView);
         nameView = findViewById(R.id.nameSettingsView);
+        surnameView = findViewById(R.id.surnameSettingsView);
+        fatherNameView = findViewById(R.id.fatherNameSettingsView);
         emailView = findViewById(R.id.emailSettingsView);
 
         database = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        storage = FirebaseStorage.getInstance();
 
-        final DatabaseReference userReference = database.getReference("Users");
+
+        userReference = database.getReference("Users");
+        storageReference = storage.getReference(currentUser.getUid());
 
         //Смена аватара
         //TODO
+        //100% Будут траблы с работой
         changeAvatarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                CustomDialogFragment dialogFragment = new CustomDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "custom");
             }
         });
 
@@ -82,14 +118,15 @@ public class SettingsActivity2 extends AppCompatActivity {
         userReference.child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String name, surname, fatherName, email;
+
                 name = dataSnapshot.child("name").getValue(String.class);
                 surname = dataSnapshot.child("surname").getValue(String.class);
                 fatherName = dataSnapshot.child("fatherName").getValue(String.class);
                 email = dataSnapshot.child("email").getValue(String.class);
 
-
-                nameView.setText(surname + " " + name + " " + fatherName);
+                nameView.setText(name);
+                surnameView.setText(surname);
+                fatherNameView.setText(fatherName);
                 emailView.setText(email);
             }
 
@@ -98,8 +135,110 @@ public class SettingsActivity2 extends AppCompatActivity {
                 Toast.makeText(SettingsActivity2.this, "Ошибка при считывании данных пользоветеля", Toast.LENGTH_LONG);
             }
         });
+        //Аватарка
+         storageReference.child("avatar.png").getStream().addOnCompleteListener(new OnCompleteListener<StreamDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<StreamDownloadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    Bitmap b;
+                    b = BitmapFactory.decodeStream(task.getResult().getStream());
+                    changeAvatarButton.setImageBitmap(b);
+                }
+                else{
+                    Toast.makeText(SettingsActivity2.this, "Ошибка при загрузке изображения из базы", Toast.LENGTH_LONG);
+                }
+            }
 
-
+        });
 
     }
+
+    //Все изменится при закрытии окна настроек
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(!nameView.getText().toString().isEmpty()){
+            userReference.child("name").setValue(nameView.getText().toString());
+        }
+        if(!surnameView.getText().toString().isEmpty()){
+            userReference.child("surname").setValue(surnameView.getText().toString());
+        }
+        if(!fatherNameView.getText().toString().isEmpty()){
+            userReference.child("fatherName").setValue(fatherNameView.getText().toString());
+        }
+        if(!emailView.getText().toString().isEmpty()){
+            userReference.child("email").setValue(emailView.getText().toString());
+        }
+    }
+    //
+
+
+    //Копипаста из ЭДД АВАТАРа
+    @Override
+    public void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { // не удалять, для снимка
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            // Фотка сделана, извлекаем картинку
+            Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
+            //user.setAvatar(thumbnailBitmap);//Не ебу, что я тут понаделал. Уебет? Не должно
+            changeAvatarInStorage(thumbnailBitmap);
+
+
+            //
+
+            changeAvatarButton.setImageBitmap(thumbnailBitmap);
+        }
+        else{
+            Bitmap bitmap = null;
+
+            switch(requestCode) {
+                case GALLERY_REQUEST:
+                    if(resultCode == RESULT_OK){
+                        Uri selectedImage = data.getData();
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        changeAvatarInStorage(bitmap);
+                        changeAvatarButton.setImageBitmap(bitmap);
+
+                    }
+            }
+        }
+        //addUserToDatabase();//Добавим пользователя в базу
+
+    }
+
+    @Override
+    public void openGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+    }
+    ///////
+
+    void changeAvatarInStorage(Bitmap avatar){
+        storageReference.child("avatar.png").delete();
+        ByteArrayOutputStream avatarBytesStream = new ByteArrayOutputStream();
+        avatar.compress(Bitmap.CompressFormat.PNG, 100, avatarBytesStream);
+        byte[] avatarBytes = avatarBytesStream.toByteArray();
+        storageReference.child(currentUser.getUid());
+        StorageReference newRef = storageReference.child(currentUser.getUid() + "/avatar.png");
+        UploadTask uploadTask = newRef.putBytes(avatarBytes);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SettingsActivity2.this, "Что-то не так с загрузкой нового изображения в базу. Проверьте соединение и повторите попытку.", Toast.LENGTH_LONG);
+
+            }
+        });
+    }
+
 }
