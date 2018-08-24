df8d5f7dee92b6d6290b25c34e6035dcf8c84a0c
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-24 12:01:04 +0300

    Тут подпись. Сам можешь заценить как и что.

---
+++ app/src/main/java/com/example/host/databaseproject/OrderRegistrationActivity.java
@@ -0,0 +1,63 @@
+package com.example.host.databaseproject;
+
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import com.kyanogen.signatureview.SignatureView;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+public class OrderRegistrationActivity extends AppCompatActivity {
+    Bitmap bitmap;
+    Button clear,save;
+    SignatureView signatureView;
+    String path; private static final String IMAGE_DIRECTORY = "/signdemo";
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_registration);
+        signatureView = (SignatureView) findViewById(R.id.signature_view);
+        clear = (Button) findViewById(R.id.clear);
+        save = (Button) findViewById(R.id.save);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                signatureView.clearCanvas();
+            } });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                bitmap = signatureView.getSignatureBitmap();
+                path = saveImage(bitmap); } });
+    }
+
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File( Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY /*iDyme folder*/);
+        // have the object build the directory structure, if needed.
+        if (!wallpaperDirectory.exists()) {
+            wallpaperDirectory.mkdirs();
+            Log.d("hhhhh",wallpaperDirectory.toString());
+        }
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance() .getTimeInMillis() + ".jpg");
+            f.createNewFile();
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(OrderRegistrationActivity.this, new String[]{f.getPath()}, new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+            return f.getAbsolutePath();
+        }
+        catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+    }
+}
+
