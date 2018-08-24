0ac6453d01d46db60e37ab10a476c1d0e4b17042
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-24 21:18:09 +0300

    Так, вроде все.

--- app/src/main/java/com/example/host/databaseproject/LoginActivity.java
+++ app/src/main/java/com/example/host/databaseproject/LoginActivity.java
@@ -28,9 +28,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ;
 
-        //startActivity(new Intent(LoginActivity.this, AccountActivity.class));
+        startActivity(new Intent(LoginActivity.this, OrderRegistrationActivity.class));
 
         mAuth = FirebaseAuth.getInstance();
 
