2d8da571f640e9b14babc8a6b5b1d4d6364bc6f1
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-25 19:19:44 +0300

    Сделал лист в каталоге. Но пока нельзя отмечать товары.

--- app/src/main/java/com/example/host/databaseproject/CatalogueFragment.java
+++ app/src/main/java/com/example/host/databaseproject/CatalogueFragment.java
@@ -7,8 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,14 +31,24 @@ public class CatalogueFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_catalogue, container, false);
 
-        final Button mainButton = (Button) rootView.findViewById(R.id.main_button);
-        mainButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        final Button mainButton = (Button) rootView.findViewById(R.id.main_button);
+//
+//        mainButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                mainButton.setBackgroundColor(getContext().getResources().getColor(R.color.colorAccent));
+//            }
+//        });
+        ListView listView = rootView.findViewById(R.id.list);
+        String[] strings = new String[3];
+        strings[0] = "Товар 1";
+        strings[1] = "Товар 2";
+        strings[2] = "Товар 3";
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_list_item_multiple_choice, strings);
 
-                mainButton.setBackgroundColor(getContext().getResources().getColor(R.color.colorAccent));
-            }
-        });
+        listView.setAdapter(adapter);
         // Inflate the layout for this fragment
         return rootView;
     }
