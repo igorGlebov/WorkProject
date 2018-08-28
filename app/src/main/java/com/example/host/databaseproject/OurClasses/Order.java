be37ec3096d1ac9f93b59f3e08508cf8b5ea3ed1
Igor Glebov <igor.glebov1999@mail.ru>
2018-08-28 15:48:08 +0300

    Класс заказа.

---
+++ app/src/main/java/com/example/host/databaseproject/OurClasses/Order.java
@@ -0,0 +1,15 @@
+package com.example.host.databaseproject.OurClasses;
+
+import java.util.ArrayList;
+
+public class Order { // заказ
+    private ArrayList products;
+
+    public ArrayList getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList products) {
+        this.products = products;
+    }
+}
